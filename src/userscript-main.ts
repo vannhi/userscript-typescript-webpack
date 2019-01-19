4af2f52cc3c43eaf4a08c474e2e128dc2358afe4
thanh <thanh@home>
2019-01-19 15:02:01 +0100

    Scaffolding

---
+++ src/userscript-main.ts
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Userscript name
+// @namespace    userscript@htnguyen.fr
+// @version      0.1
+// @description  Take over the world
+// @author       You
+// @include      https://www.google.com/*
+// @grant        none
+// @noframes
+// ==/UserScript==
+
+(function() {
+  console.log("Congratulation! You took over the world!");
+})();
---
+++ webpack.config.ts
@@ -0,0 +1,52 @@
+import webpack from "webpack";
+import path from "path";
+import fs from "fs";
+
+const TerserPlugin = require('terser-webpack-plugin');
+
+const config: webpack.Configuration = {
+  entry: "./src/userscript-main.ts",
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          output: {
+            comments: /(\s@|UserScript==)/i,
+          },
+        }
+      })
+    ]
+  },
+  module: {
+    rules: [{
+      test: /\.ts$/,
+      exclude: /node_modules/,
+      use: {
+        loader: "ts-loader",
+        options: {
+          transpileOnly: true
+        }
+      }
+    }]
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "script.user.js"
+  },
+  resolve: {
+    modules: [
+      "node_modules",
+      "src"
+    ],
+    extensions: [".ts"],
+  },
+  plugins: [
+    new webpack.BannerPlugin({
+      banner: fs.readFileSync(path.resolve(__dirname, "src/userscript-main.ts"), "utf-8").replace(/(==\/UserScript==)[\s\S]+$/, "$1"),
+      entryOnly: true,
+      raw: true
+    })
+  ]
+};
+
+export default config;
